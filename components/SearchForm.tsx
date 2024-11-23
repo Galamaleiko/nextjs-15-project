8efcaeb682c1543fc4b031501d13dd7d7c3c4c63
Serhii <sergijba@gmail.com>
2024-11-23 14:25:42 +0100

    Search form finalisation and search query implementation

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import Form from 'next/form'
 import SearchFormReset from './SearchFormReset'
+import { Search } from 'lucide-react'
 
-const SearchForm = () => {
-    const query = 
-
+const SearchForm = ({ query }: { query?:string}) => {
 
     return (
-        <div className=''>
+        <div>
             <Form action='/' scroll={false} className='search-form'>
                 <input name='query' defaultValue={query} className='search-input' placeholder='Search Startups' />
 
                 <div className='flex gap-2'>
                     {query && <SearchFormReset />}
-                    <button type='submit' className='search-btn'></button>
+                    <button type='submit' className='search-btn text-white'>
+                        <Search className='size-6'/>
+                    </button>
                 </div>
             </Form>
         </div>
