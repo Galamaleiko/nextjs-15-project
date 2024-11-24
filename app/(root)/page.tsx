a131b5d2bdcea310f5a484e96fd6d2304dd4b492
Serhii <sergijba@gmail.com>
2024-11-24 14:04:45 +0100

    Sanity initialization

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,30 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
   searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query;
+
+  const posts = [{
+    _createdAt: new Date(),
+    views: 56,
+    author: { _id: 1, name: 'George Ambington' },
+    _id: 1,
+    description: 'This is a description.',
+    image: 'https://images.pexels.com/photos/2558605/pexels-photo-2558605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    category: "Animals",
+    title: "Grey kitten"
+  }, {
+    _createdAt: new Date(),
+    views: 56,
+    author: { _id: 2, name: 'Sofia Karovski' },
+    _id: 2,
+    description: 'This is a description 2.',
+    image: 'https://images.pexels.com/photos/2558605/pexels-photo-2558605.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+    category: "Animals",
+    title: "Cute kitten"
+  }]
 
   return (
     <>
@@ -16,6 +37,20 @@ export default async function Home({ searchParams }: {
 
         <SearchForm query={query} />
 
+      </section>
+
+      <section className="section_container" >
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "Show all startups"}
+        </p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts?.map((post: StartupCardType, number) => (
+              <StartupCard key={post?._id} post = {post} />
+            ))) : (
+            <p className="no-results"> No startups found </p>
+          )}
+        </ul>
       </section>
 
     </>
