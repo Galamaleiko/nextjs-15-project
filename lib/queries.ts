6c3ef3c683373764fa4488c8521e10090c51946e
Serhii <sergijba@gmail.com>
2024-11-27 13:52:21 +0100

    Sanity Write Client + Author Authentication implementation

--- lib/queries.ts
+++ lib/queries.ts
@@ -24,3 +24,18 @@ export const STARTUP_BY_ID_QUERY = defineQuery(`*[_type == 'startup' && _id matc
     image,
     pitch
 }`)
+
+export const STARTUP_VIEWS_BY_ID_QUERY = defineQuery(`*[_type == 'startup' && _id == $id][0]{
+    _id,
+    views
+}`)
+
+export const AUTHOR_BY_GITHUB_ID_QUERY = defineQuery(`*[_type == 'author' && id == $id][0]{
+    _id,
+    id,
+    name,
+    username,
+    email,
+    image,
+    bio    
+}`)
